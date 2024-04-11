33515621a07ab441e0e679b8d399bb7da574bfe6
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-11 11:24:21 -0300

    feat: page gerar sinais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
 
       <div className="container mx-auto flex justify-center py-20 mt-20">
         <div className="flex flex-col justify-center items-center gap-2">
-        <div className="w-20 sm:w-40">
-          <Image width={9000} height={9000} src={logoFP} alt="Logo Oficial FP" />
-        </div>
+          <div className="w-20 sm:w-40">
+            <Image width={9000} height={9000} src={logoFP} alt="Logo Oficial FP" />
+          </div>
           <h1 className="text-center text-xl sm:text-2xl">FP PORCENTAGEM DE PAGAMENTO</h1>
-      </div>
+        </div>
       </div>
 
       <section className="">
