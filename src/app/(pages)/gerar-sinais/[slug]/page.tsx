8e2992f7dea9416bf6c5fc859462cb33c5fb2eba
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-18 09:53:30 -0300

    feat: posição sinais

--- src/app/(pages)/gerar-sinais/[slug]/page.tsx
+++ src/app/(pages)/gerar-sinais/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { gamesSinais } from "@/lib/constants"
 import { cn } from "@/lib/utils";
-
+import iconEntrada from '../../../../../public/image/icone-sinais/iconentrada.png'
+import iconTurbo from '../../../../../public/image/icone-sinais/iconturbo.png'
+import iconValidade from '../../../../../public/image/icone-sinais/iconvalidade.png'
+import Image from "next/image";
 
 const backgroundMap: Record<string, string> = {
     "fortune-ox": "bg-fortune-ox",
@@ -17,8 +20,54 @@ export default function PageGamesSinais({ params }: { params: { slug: string } }
     const game = gamesSinais.find(signal => signal.slug === params.slug);
 
     return (
-        <div>
-            <div className={cn("w-full h-[calc(100vh-80px)] mt-20 bg-center bg-contain bg-no-repeat", backgroundMap[game!.slug])} />
+        <div className={cn("w-full h-[640px] mt-[73px] bg-center bg-contain bg-no-repeat flex items-center justify-center", backgroundMap[game!.slug])}>
+            <div className="w-[395px] h-[375px] mt-[82px] flex flex-col justify-between px-3 py-1 border-zinc-50 border">
+                <div className="p-2 w-full flex flex-row border-[#9d2525] border rounded-lg items-center ">
+                    <div className="w-14 items-start ">
+                        <Image
+                            width={900}
+                            height={900}
+                            src={iconEntrada}
+                            alt="Icon Entrada"
+                            className=""
+                        />
+                    </div>
+                    <div className="flex ml-5 text-[#9d2525] text-2xl font-bold gap-4 uppercase py-6">
+                        <h1 className="">NORMAL:</h1>
+                        <span>5x</span>
+                    </div>
+                </div>
+                <div className="p-2 w-full flex flex-row border-[#9d2525] border rounded-lg items-center ">
+                    <div className="w-14 items-start ">
+                        <Image
+                            width={900}
+                            height={900}
+                            src={iconTurbo}
+                            alt="Icon Entrada"
+                            className=""
+                        />
+                    </div>
+                    <div className="flex ml-5 text-[#9d2525] text-2xl font-bold gap-4 uppercase py-6">
+                        <h1 className="">TURBO:</h1>
+                        <span>5x</span>
+                    </div>
+                </div>
+                <div className="p-2 w-full flex flex-row border-[#9d2525] border rounded-lg items-center ">
+                    <div className="w-14 items-start ">
+                        <Image
+                            width={900}
+                            height={900}
+                            src={iconValidade}
+                            alt="Icon Entrada"
+                            className=""
+                        />
+                    </div>
+                    <div className="flex ml-5 text-[#9d2525] text-2xl font-bold gap-4 uppercase py-6">
+                        <h1 className="">JOGUE ATÉ:</h1>
+                        <span>10:00</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -6,7 +6,7 @@ export function NavBar() {
         <div>
             <nav className="shadow-sm bg-zinc-900 border-b border-zinc-500/20 fixed top-0 w-full z-50">
                 <div className="container mx-auto flex flex-col items-center justify-center gap-5 py-3 px-4 sm:px-0 text-base lg:px-8">
-                    <div className="flex flex-row gap-10 text-lg w-full justify-center">
+                    <div className="flex flex-row gap-2 sm:gap-10 text-lg w-full justify-center">
                         <Link href="/" className="hover:font-bold w-[200px] py-2 text-center border border-zinc-700 hover:bg-zinc-700 rounded-md transition-all duration-200 text-base sm:text-xl">
                             PORCENTAGEM
                         </Link>
