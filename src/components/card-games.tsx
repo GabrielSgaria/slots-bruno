4825b87695a1d0f4165efce5df39d831b662df83
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-19 15:24:46 -0300

    feat: color

--- src/components/card-games.tsx
+++ src/components/card-games.tsx
@@ -22,7 +22,7 @@ export async function CardGames({ linkCasa }: CardGamesProps) {
                             <p className='font-bold text-center text-sm z-20 absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-zinc-950'>{porcentagem}%</p>
                             <div className={`h-full imagebg`} style={{
                                 width: `${porcentagem}%`,
-                                background: `${corRandom}`,
+                                background: `${getRandomColor()}`,
                                 backgroundImage: `linear-gradient(90deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent)`,
                                 backgroundSize: `200% 100%`,
                                 transition: `background-position-x 1s ease`,
