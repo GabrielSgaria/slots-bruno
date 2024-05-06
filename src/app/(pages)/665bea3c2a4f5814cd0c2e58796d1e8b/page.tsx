52094e94db82711352e6dad4cdf7311b8613ef5c
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-06 11:14:15 -0300

    feat: responsive adm

--- src/app/(pages)/665bea3c2a4f5814cd0c2e58796d1e8b/page.tsx
+++ src/app/(pages)/665bea3c2a4f5814cd0c2e58796d1e8b/page.tsx
@@ -9,7 +9,7 @@ export default async function PageAdmin() {
     })
     return (
         <div className="bg-zinc-50 h-screen w-full">
-            <div className="container mx-auto flex justify-center pt-28 flex-col items-center">
+            <div className="container mx-auto flex justify-center pt-28 flex-col items-center px-2 sm:px-0 ">
                 <h1 className="text-zinc-950 text-2xl uppercase">FP - SINAIS ADMIN</h1>
                 <FormAdm linkAtual={settings?.link || ""}/>
             </div>
--- src/components/form-adm.tsx
+++ src/components/form-adm.tsx
@@ -17,23 +17,23 @@ export function FormAdm({ linkAtual }: { linkAtual: string }) {
     };
 
     return (
-        <form action={handleFormSubmit} className="mt-10 flex flex-col border border-zinc-300 py-20 px-10 w-[900px] h-[450px] rounded-2xl gap-5 items-center">
+        <form action={handleFormSubmit} className="mt-10 flex flex-col border border-zinc-300 py-20 px-10 w-full lg:w-[900px] h-[450px] rounded-2xl gap-5 items-center">
             <input
                 defaultValue={linkAtual}
                 name="link"
                 type="text"
                 placeholder="Digite o novo link..."
-                className="border border-zinc-300 rounded-2xl px-3 text-zinc-950 w-9/12 h-20 shadow-md"
+                className="border border-zinc-300 rounded-2xl px-3 text-zinc-950 w-full h-20 shadow-md text-xs md:text-base"
             />
             <input
                 name="hash"
                 type="text"
                 placeholder="Digite a hash de validação..."
-                className="border border-zinc-300 rounded-2xl px-3 text-zinc-950 w-9/12 h-20 shadow-md"
+                className="border border-zinc-300 rounded-2xl px-3 text-zinc-950 w-full h-20 shadow-md text-xs md:text-base"
             />
             <button
                 type="submit"
-                className={`mt-10 bg-green-400 hover:bg-green-600 transition-all rounded-2xl text-zinc-950 h-20 px-3 w-9/12 shadow-md text-center flex items-center justify-center font ${isPending ? 'cursor-not-allowed' : ''}`}
+                className={`mt-10 bg-green-400 hover:bg-green-600 transition-all rounded-2xl text-zinc-950 h-20 px-3 w-full shadow-md text-center flex items-center justify-center font ${isPending ? 'cursor-not-allowed' : ''}`}
                 disabled={isPending}
             >
                 {isPending ? <SymbolIcon className="size-6 animate-spin" /> : 'ALTERAR LINK'}
