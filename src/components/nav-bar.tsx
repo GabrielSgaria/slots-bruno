361f85af302fc1703dd673bf638332fb27dcfc9f
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-27 13:25:51 -0300

    feat: nav-bar division

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -8,15 +8,17 @@ export async function NavBar() {
         <div>
             <nav className="shadow-sm shadow-black bg-transparent/95 border-b border-zinc-500/20 fixed top-0 w-full z-50">
                 <div className="container mx-auto flex flex-col items-center justify-center gap-5 py-3 px-2">
-                    <div className="flex flex-row gap-2 sm:gap-10 text-lg w-full justify-center">
+                    <div className="flex flex-row gap-2 sm:gap-10 text-lg w-full justify-center items-center">
                         <Link href="/" className=" py-0 text-center justify-center items-center flex text-zinc-950 font-bold rounded-md transition-all duration-200  sm:text-base">
-                            <p className="text-zinc-50 hover:underline text-sm">PORCENTAGEM</p>
+                            <p className="text-zinc-50 hover:underline text-xs sm:text-sm">PORCENTAGEM</p>
                         </Link>
+                        <div className="h-10 border-r border-zinc-50/20"/>
                         <Link href="/gerar-sinais" className="py-0 text-center justify-center items-center flex text-zinc-950 font-bold rounded-md transition-all duration-200  sm:text-base">
-                            <p className="text-zinc-50 hover:underline text-sm">GERAR SINAIS</p>
+                            <p className="text-zinc-50 hover:underline text-xs sm:text-sm">GERAR SINAIS</p>
                         </Link>
+                        <div className="h-10 border-r border-zinc-50/20"/>
                         {novoLink.data ? (
-                            <Link href={novoLink?.data} target='_blank' className="text-sm italic font-bold py-2 px-2 text-center bg-green-700 hover:bg-green-600 text-yellow-400 rounded-md transition-all duration-200 sm:text-base">
+                            <Link href={novoLink?.data} target='_blank' className="h-full text-xs sm:text-sm italic font-bold py-2 px-2 text-center bg-green-700 hover:bg-green-600 text-yellow-400 rounded-md transition-all duration-200">
                                 JOGUE AGORA
                             </Link>
                         ) :
