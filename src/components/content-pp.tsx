58004fa1824fb26e1a4f3d60de980aadc30d80cd
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-04 16:37:32 -0300

    w-36

--- src/components/content-pp.tsx
+++ src/components/content-pp.tsx
@@ -15,7 +15,7 @@ export function ContentPp({ updateTime }: ContentPpProps) {
              <div className="container mx-auto flex justify-center pt-28 flex-col items-center px-3 sm:px-0 gap-10 mb-10">
                 <div className="flex flex-col justify-center items-center gap-2 backdrop-blur-sm bg-green-700/75 shadow-2xl shadow-black rounded-2xl py-5 px-10">
 
-                    <div className="w-20">
+                    <div className="w-36">
                             <Image
                                 src='https://sa-east-1.graphassets.com/clxhh2irf0i1g0ekkf9ad5xah/cly7msmbs063607kabe7ayoud'
                                 width={1980}
