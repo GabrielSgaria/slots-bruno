3779d21ad635a0a58ce7a1d6a4041a0e8af5c252
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-12 15:36:47 -0300

    Atualização, qualidade imagens e hot cards

--- src/app/(pages)/gerar-sinais/[slug]/page.tsx
+++ src/app/(pages)/gerar-sinais/[slug]/page.tsx
@@ -1,18 +1,18 @@
-'use client'
-import { gamesSinais } from "@/lib/constants"
+'use client';
+import { gamesSinais } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import Image, { StaticImageData } from "next/image";
-import tigerImage from '../../../../../public/image/bg-sinais/new-tiger.png'
-import dragonImage from '../../../../../public/image/bg-sinais/new-dragon.png'
-import oxImage from '../../../../../public/image/bg-sinais/new-ox.png'
-import rabbitImage from '../../../../../public/image/bg-sinais/new-rabbit.png'
-import mouseImage from '../../../../../public/image/bg-sinais/new-mouse.png'
-import iconEntrada from '../../../../../public/image/icone-sinais/iconentrada.png'
-import tBranco from '../../../../../public/image/icone-sinais/iconturboBranco.png'
-import vBranco from '../../../../../public/image/icone-sinais/iconvalidadeBranco.png'
+import tigerImage from '../../../../../public/image/bg-sinais/new-tiger.png';
+import dragonImage from '../../../../../public/image/bg-sinais/new-dragon.png';
+import oxImage from '../../../../../public/image/bg-sinais/new-ox.png';
+import rabbitImage from '../../../../../public/image/bg-sinais/new-rabbit.png';
+import mouseImage from '../../../../../public/image/bg-sinais/new-mouse.png';
+import iconEntrada from '../../../../../public/image/icone-sinais/iconentrada.png';
+import tBranco from '../../../../../public/image/icone-sinais/iconturboBranco.png';
+import vBranco from '../../../../../public/image/icone-sinais/iconvalidadeBranco.png';
 import tBege from '../../../../../public/image/icone-sinais/iconturboBege.png';
-import vBege from '../../../../../public/image/icone-sinais/iconvalidadeBege.png'
-import { DotFilledIcon, GearIcon } from "@radix-ui/react-icons";
+import vBege from '../../../../../public/image/icone-sinais/iconvalidadeBege.png';
+import { DotFilledIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import { generateRandomNumber, calculateEndTime } from "@/lib/utils";
 import { CircleNotch } from "@phosphor-icons/react/dist/ssr";
@@ -23,7 +23,7 @@ const imageMap: Record<string, StaticImageData> = {
     'fortune-tiger': tigerImage,
     'fortune-mouse': mouseImage,
     'fortune-dragon': dragonImage,
-}
+};
 
 const colorBgMap: Record<string, string> = {
     'fortune-ox': "bg-fortune-ox-color",
@@ -31,25 +31,19 @@ const colorBgMap: Record<string, string> = {
     'fortune-tiger': 'bg-fortune-tiger-color',
     'fortune-mouse': 'bg-fortune-mouse-color',
     'fortune-dragon': 'bg-fortune-dragon-color',
-}
+};
 
-const borderMap: Record<string, string> = {
-    'fortune-ox': "border-fortune-ox-color",
-    'fortune-rabbit': 'border-fortune-rabbit-color',
-    'fortune-tiger': 'border-fortune-tiger-color',
-    'fortune-mouse': 'border-fortune-mouse-color',
-    'fortune-dragon': 'border-fortune-dragon-color',
-}
 const textMap: Record<string, string> = {
     "fortune-ox": "text-fortune-ox-color",
     'fortune-rabbit': 'text-fortune-rabbit-color',
     'fortune-tiger': 'text-fortune-tiger-color',
     'fortune-mouse': 'text-fortune-mouse-color',
-    'fortune-dragon': 'text-fortune-dragon-color'
-}
+    'fortune-dragon': 'text-fortune-dragon-color',
+};
 
 export default function PageGamesSinais({ params }: { params: { slug: string } }) {
-    const game = gamesSinais.find(signal => signal.slug === params.slug);
+    const gameName = params.slug ? params.slug.toString().replace(/%20/g, '-').toLowerCase() : '';
+    const game = gamesSinais.find(signal => signal.slug === gameName);
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [message, setMessage] = useState("");
     const [normalValue, setNormalValue] = useState("--");
@@ -83,32 +77,30 @@ export default function PageGamesSinais({ params }: { params: { slug: string } }
         }, 5000);
     };
 
+    if (!game) {
+        return <p className="text-center text-red-500">Jogo não encontrado</p>;
+    }
+
     return (
         <div className={cn("w-full flex flex-col items-center justify-center")}>
             <div className="min-h-[55px] sm:min-h-[65px] w-full" />
             <div className="flex h-[90vh] items-center justify-center p-3">
-
-                <div className={cn("max-w-[340px] max-h-[490px] sm:max-w-[400px] sm:max-h-[590px] min-h-[400px] w-full h-full flex flex-col justify-between rounded-2xl shadow-2xl shadow-black backdrop-blur-2xl border border-zinc-600 overflow-hidden bg-opacity-20", `${colorBgMap[game!.slug]}`)}>
+                <div className={cn("max-w-[340px] max-h-[490px] sm:max-w-[400px] sm:max-h-[590px] min-h-[400px] w-full h-full flex flex-col justify-between rounded-2xl shadow-2xl shadow-black backdrop-blur-2xl border border-zinc-600 overflow-hidden bg-opacity-20", `${colorBgMap[gameName]}`)}>
                     <div className="flex flex-col">
-
                         <div className='h-full max-h-[150px] min-h-[100px] w-full'>
-                            {game && imageMap[game.slug] && (
-                                <Image
-                                    width={9000}
-                                    height={9000}
-                                    quality={100}
-                                    priority={true}
-                                    src={imageMap[game.slug]}
-                                    alt={game.slug}
-                                    className="object-center w-full h-full"
-                                />
-                            )}
+                            <Image
+                                width={9000}
+                                height={9000}
+                                quality={100}
+                                priority={true}
+                                src={imageMap[game.slug]}
+                                alt={game.slug}
+                                className="object-center w-full h-full"
+                            />
                         </div>
-
                         <p className="backdrop-blur-sm text-center font-bold uppercase bg-zinc-950/30">
-                            {game!.slug}
+                            {gameName}
                         </p>
-
                     </div>
                     <div className="flex flex-col px-5 h-full justify-around my-2">
                         <div className={cn("px-3 py-1 w-full flex flex-row border border-zinc-950/20 rounded-lg items-center backdrop-blur-sm bg-zinc-950/20 shadow-lg shadow-black")}>
@@ -126,13 +118,12 @@ export default function PageGamesSinais({ params }: { params: { slug: string } }
                                 <span>{normalValue}</span>
                             </div>
                         </div>
-                        <div className={cn("py-1 px-3 w-full flex flex-row border rounded-lg items-center border-zinc-950/20 backdrop-blur-sm bg-zinc-950/20 shadow-lg shadow-black", textMap[game!.slug])}>
+                        <div className={cn("py-1 px-3 w-full flex flex-row border rounded-lg items-center border-zinc-950/20 backdrop-blur-sm bg-zinc-950/20 shadow-lg shadow-black", textMap[gameName])}>
                             <div className="w-12  justify-center flex">
                                 <Image
                                     width={900}
                                     height={900}
                                     src={tBranco}
-                                    // src={game && game.slug === 'fortune-rabbit' || game && game.slug === 'fortune-tiger' || game && game.slug === 'fortune-dragon' ? tBege : tBranco}
                                     alt="Icon Entrada"
                                     className="w-10/12"
                                 />
@@ -148,10 +139,8 @@ export default function PageGamesSinais({ params }: { params: { slug: string } }
                                     width={900}
                                     height={900}
                                     src={vBranco}
-                                    // src={game && game.slug === 'fortune-rabbit' || game && game.slug === 'fortune-tiger' || game && game.slug === 'fortune-dragon' ? vBege : vBranco}
                                     alt="Icon Entrada"
                                     className="w-10/12"
-
                                 />
                             </div>
                             <div className={cn("flex ml-5 text-lg sm:text-lg font-bold gap-4 uppercase py-3 sm:py-6 text-zinc-50")}>
@@ -181,5 +170,5 @@ export default function PageGamesSinais({ params }: { params: { slug: string } }
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/components/card-games.tsx
+++ src/components/card-games.tsx
@@ -1,9 +1,9 @@
 'use client';
 import Image from 'next/image';
-import { cn, getRandomClass } from '@/lib/utils';
-import { useState, useEffect } from 'react';
-import { MoneyWavy } from '@phosphor-icons/react';
+import { cn } from '@/lib/utils';
+import { Fire, MoneyWavy } from '@phosphor-icons/react';
 import { allImageCards } from '@/lib/images-cards';
+import Link from 'next/link';
 
 interface CardGamesProps {
     linkCasa?: string | null;
@@ -16,15 +16,13 @@ interface CardGamesProps {
 }
 
 export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, nomeJogo }: CardGamesProps) {
-    const [colorMinima, setColorMinima] = useState<string>('');
-    const [colorPadrao, setColorPadrao] = useState<string>('');
-    const [colorMaxima, setColorMaxima] = useState<string>('');
+    const getColor = (value: number) => {
+        if (value >= 70) return 'bg-green-500';
+        if (value >= 40) return 'bg-yellow-500';
+        else return 'bg-red-600';
+    }
 
-    useEffect(() => {
-        setColorMinima(getRandomClass());
-        setColorPadrao(getRandomClass());
-        setColorMaxima(getRandomClass());
-    }, []);
+    const isHot = nomeJogo.toLowerCase().startsWith('fortune') && (minima > 90 || padrao > 90 || maxima > 90);
 
     if (!linkCasa) {
         return <p>Link não encontrado</p>;
@@ -32,27 +30,33 @@ export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, n
 
     return (
         <div key={id} className="flex flex-col my-4 justify-between backdrop-blur-md bg-zinc-950/20 border border-zinc-950/5 shadow-xl shadow-black px-0 pb-0 rounded-3xl max-w-[200px] min-w-[197px] overflow-hidden">
+            {isHot && (
+                <div className="absolute top-1 right-1 z-20 w-[70px] h-7 rounded-md flex items-center justify-center bg-red-600 animate-pulse">
+                    <p className='text-zinc-50 text-md font-bold'>HOT</p>
+                    <Fire weight="fill" className="text-red-50 text-xl animate-pulse" />
+                </div>
+            )}
             <a href={linkCasa} target='_blank' className='sm:hover:opacity-60 h-full'>
                 <Image
-                    width={198}
-                    height={180}
+                    width={270}
+                    height={270}
                     src={allImageCards[id]}
                     alt={`Card ${id}`}
-                    className="w-full h-full object-fill min-h-[180px] max-h-[180px]  bg-zinc-950"
+                    className="w-full h-full object-fill min-h-[150px] max-h-[150px] bg-zinc-950"
                     loading="lazy"
                     quality={100}
                     placeholder="blur"
                     blurDataURL={allImageCards[id]}
                 />
             </a>
-            <div className='w-full h-full backdrop-blur-md py-2 px-4 flex flex-col items-start justify-start'>
+            <div className='w-full max-h-[32px] h-full backdrop-blur-md py-2 px-4 flex flex-col items-start justify-start'>
                 <p className='w-full text-xs justify-center text-center'>Distribuição: {porcentagem}%</p>
             </div>
             <div className='px-2 h-full w-full flex flex-col gap-1 my-5'>
                 {[
-                    { label: 'Aposta Mínima', value: minima, color: colorMinima },
-                    { label: 'Aposta Padrão', value: padrao, color: colorPadrao },
-                    { label: 'Aposta Máxima', value: maxima, color: colorMaxima },
+                    { label: 'Aposta Mínima', value: minima, color: getColor(minima) },
+                    { label: 'Aposta Padrão', value: padrao, color: getColor(padrao) },
+                    { label: 'Aposta Máxima', value: maxima, color: getColor(maxima) },
                 ].map(({ label, value, color }) => (
                     <div key={label} className='flex flex-col'>
                         <p className='text-center text-xs flex gap-2 items-center justify-center mb-1 font-semibold'>{label}:</p>
@@ -75,7 +79,7 @@ export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, n
             <a href={linkCasa} target='_blank' className="mb-2 mx-2 hover:underline border border-zinc-50/15 text-center justify-center bg-zinc-50/10 hover:bg-zinc-50/20 items-center flex font-bold rounded-md transition-all text-base sm:text-base">
                 <p className="text-zinc-50">JOGUE AGORA</p>
             </a>
-            <div className="w-full h-full backdrop-blur-md pt-2 pb-4 px-2 mt-2 flex flex-col items-start justify-start text-nowrap">
+            <div className="w-full h-full backdrop-blur-md pt-2 pb-1 px-2 mt-2 flex flex-col items-center justify-center text-nowrap">
                 {[
                     { label: 'Mínima R$0,50 a R$2,40' },
                     { label: 'Padrão R$2,50 a R$10,00' },
@@ -86,6 +90,15 @@ export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, n
                     </p>
                 ))}
             </div>
+            {isHot && (
+                <div className="w-full items-center flex justify-center py-2 px-1">
+                    <Link
+                        href={`/gerar-sinais/${nomeJogo}`}
+                        className="bg-red-700 text-zinc-50 font-bold py-3 px-1 rounded-xl hover:bg-red-700 w-full text-center text-nowrap text-sm sm:text-base animate-pulse">
+                        Estratégia detectada
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
