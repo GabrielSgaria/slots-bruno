c896de3c313ae0e64f108ac70e73c76c8e37b68b
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-22 21:38:03 -0300

    feat image bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { NavBar } from "@/components/nav-bar";
 import { Footer } from "@/components/footer";
 import { Analytics } from "@vercel/analytics/next";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import Image from "next/image";
 
 
 
@@ -63,7 +64,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt">
+    <html lang="pt_br">
+      <head>
+
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '842834544656086');
+            fbq('track', 'PageView');
+          `
+        }} />
+        <noscript>
+          <Image alt='pixel' height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=842834544656086&ev=PageView&noscript=1" />
+        </noscript>
+      </head>
       <body className={`bg-image-mobile md:bg-image-desktop bg-fixed bg-contain md:bg-cover bg-center-top text-zinc-50 ${roboto.variable}`}>
         <Analytics />
         <GoogleAnalytics gaId="G-9E7Z61LW2J" />
