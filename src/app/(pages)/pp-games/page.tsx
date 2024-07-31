c56d9c81a44450d573be1e410ce788208a275eca
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-31 08:28:12 -0300

    feat: images

--- src/app/(pages)/pp-games/page.tsx
+++ src/app/(pages)/pp-games/page.tsx
@@ -27,20 +27,9 @@ export default async function PpGames() {
     revalidateTag('timeCron')
     return (
         <main>
-
-            <div className="h-screen w-full flex items-center justify-center bg-green-950 flex-col gap-10">
-                <h1 className="font-bold text-2xl text-yellow-500">PÁGINA EM MANUTENÇÃO</h1>
-                <div className="w-24 flex items-center justify-center relative ">
-                    <div className="bg-green-900 p-2 rounded-xl animate-ping absolute w-20 h-20 z-10" />
-                    <Image src={logoFp} alt="Logo FP" width={900} height={900} quality={100} className="z-20" />
-                </div>
-            </div>
-
-
-
-            {/* <ButtonScrollTop />
+            <ButtonScrollTop />
             <ContentPp updateTime={formattedDate} />
-            <SectionCardsPP cards={cards?.data as CardData[]} linkCasa={novoLink} /> */}
+            <SectionCardsPP cards={cards?.data as CardData[]} linkCasa={novoLink} />
         </main>
     )
 } 
