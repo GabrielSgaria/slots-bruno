514f9c65064957e0898c635cb77c8ca2ca8bcd73
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-08-05 10:22:18 -0300

    card game

--- src/components/card-games.tsx
+++ src/components/card-games.tsx
@@ -30,13 +30,8 @@ export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, n
     }
 
     return (
-        <div key={id} className="flex flex-col my-4 justify-between backdrop-blur-md bg-zinc-950/20 border border-zinc-950/5 shadow-xl shadow-black px-0 pb-0 rounded-xl max-w-[215px] sm:max-w-[175px] w-full sm:min-w-[157px] overflow-hidden">
-            {isHot && (
-                <div className="absolute top-1 right-1 z-20 w-[70px] h-7 rounded-md flex items-center justify-center bg-red-600 animate-pulse">
-                    <p className='text-zinc-50 text-md font-bold'>HOT</p>
-                    <Fire weight="fill" className="text-red-50 text-xl animate-pulse" />
-                </div>
-            )}
+
+        <div key={id} className='shadow-xl h-auto w-full px-[5px] text-white mb-4 relative bg-indigo-700/30 '>
             <a href={linkCasa} target='_blank' className='sm:hover:opacity-60 h-full'>
                 <Image
                     width={370}
@@ -44,67 +39,91 @@ export function CardGames({ linkCasa, id, porcentagem, minima, padrao, maxima, n
                     src={allImageCards[id]}
                     alt={`Card ${id}`}
                     className="object-cover min-h-[140px] max-h-[150px] bg-zinc-950"
-                    // loading="lazy"
                     priority={true}
                     quality={100}
-                // placeholder="blur"
-                // blurDataURL={allImageCards[id]}
                 />
             </a>
-            <div className={cn(`flex flex-col justify-between`)} style={{ backgroundImage: `url(${allImageCards[id]})`, opacity: 0.8 }}>
-                <div className='w-full h-full flex flex-col bg-indigo-600/30 backdrop-blur-xl '>
-                    <div className='w-full max-h-[32px] h-full backdrop-blur-md py-2 px-4 flex flex-col items-start justify-start'>
-                        <p className='w-full text-xs justify-center text-center'>Distribuição {porcentagem}%</p>
-                    </div>
-                    <div className='px-2 h-full w-full flex flex-col gap-1 my-5'>
-                        {[
-                            { label: 'Aposta Mínima', value: minima, color: getColor(minima) },
-                            { label: 'Aposta Padrão', value: padrao, color: getColor(padrao) },
-                            { label: 'Aposta Máxima', value: maxima, color: getColor(maxima) },
-                        ].map(({ label, value, color }) => (
-                            <div key={label} className='flex flex-col'>
-                                <p className='text-center text-xs flex gap-2 items-center justify-center mb-1 font-semibold'>{label}:</p>
-                                <div className={`w-full h-3 rounded-full bg-zinc-50 relative z-10 overflow-hidden`}>
-                                    <p className='font-bold text-center text-xs z-20 absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-zinc-950'>{value}%</p>
-                                    <div
-                                        className={cn(`h-full`, color)}
-                                        style={{
-                                            width: `${value}%`,
-                                            backgroundImage: `linear-gradient(120deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent)`,
-                                            backgroundSize: `200% 100%`,
-                                            transition: `background-position-x 1s ease`,
-                                            animation: `progress-bar-stripes 6s linear infinite`,
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <a href={linkCasa} target='_blank' className="mb-2 mx-2 hover:underline border border-zinc-50/15 text-center justify-center bg-zinc-50/10 hover:bg-zinc-50/20 items-center flex font-bold rounded-md transition-all text-base sm:text-base">
-                        <p className="text-zinc-50">JOGUE AGORA</p>
-                    </a>
-                    <div className="w-full h-full backdrop-blur-md pt-2 pb-1 px-2 mt-2 flex flex-col items-center justify-center text-nowrap">
-                        {[
-                            { label: 'Mínima R$0,50 a R$2,40' },
-                            { label: 'Padrão R$2,50 a R$10,00' },
-                            { label: 'Máxima acima de R$10,00' },
-                        ].map(({ label }) => (
-                            <p key={label} className="text-start text-xs flex gap-1 items-center justify-start">
-                                <MoneyWavy /> {label}
-                            </p>
-                        ))}
-                    </div>
-                    {isHot && (
-                        <div className="w-full items-center flex justify-center py-2 px-1">
-                            <Link
-                                href={`/gerar-sinais/${nomeJogo}`}
-                                className="bg-red-700 text-zinc-50 font-bold py-3 px-1 rounded-xl hover:bg-red-700 w-full text-center text-nowrap text-sm sm:text-base animate-pulse">
-                                Estratégia detectada
-                            </Link>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </div >
+        </div>
+
     );
 }
+
+
+
+
+{/* <div key={id} className="flex flex-col my-4 justify-between backdrop-blur-md bg-zinc-950/20 border border-zinc-950/5 shadow-xl shadow-black px-0 pb-0 rounded-xl max-w-[215px] sm:max-w-[175px] w-full sm:min-w-[157px] overflow-hidden"> */ }
+//     {isHot && (
+//         <div className="absolute top-1 right-1 z-20 w-[70px] h-7 rounded-md flex items-center justify-center bg-red-600 animate-pulse">
+//             <p className='text-zinc-50 text-md font-bold'>HOT</p>
+//             <Fire weight="fill" className="text-red-50 text-xl animate-pulse" />
+//         </div>
+//     )}
+//     <a href={linkCasa} target='_blank' className='sm:hover:opacity-60 h-full'>
+//         <Image
+//             width={370}
+//             height={370}
+//             src={allImageCards[id]}
+//             alt={`Card ${id}`}
+//             className="object-cover min-h-[140px] max-h-[150px] bg-zinc-950"
+//             // loading="lazy"
+//             priority={true}
+//             quality={100}
+//         // placeholder="blur"
+//         // blurDataURL={allImageCards[id]}
+//         />
+//     </a>
+//     <div className={cn(`flex flex-col justify-between`)} style={{ backgroundImage: `url(${allImageCards[id]})`, opacity: 0.8 }}>
+//         <div className='w-full h-full flex flex-col bg-indigo-600/30 backdrop-blur-xl '>
+//             <div className='w-full max-h-[32px] h-full backdrop-blur-md py-2 px-4 flex flex-col items-start justify-start'>
+//                 <p className='w-full text-xs justify-center text-center'>Distribuição {porcentagem}%</p>
+//             </div>
+//             <div className='px-2 h-full w-full flex flex-col gap-1 my-5'>
+//                 {[
+//                     { label: 'Aposta Mínima', value: minima, color: getColor(minima) },
+//                     { label: 'Aposta Padrão', value: padrao, color: getColor(padrao) },
+//                     { label: 'Aposta Máxima', value: maxima, color: getColor(maxima) },
+//                 ].map(({ label, value, color }) => (
+//                     <div key={label} className='flex flex-col'>
+//                         <p className='text-center text-xs flex gap-2 items-center justify-center mb-1 font-semibold'>{label}:</p>
+//                         <div className={`w-full h-3 rounded-full bg-zinc-50 relative z-10 overflow-hidden`}>
+//                             <p className='font-bold text-center text-xs z-20 absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-zinc-950'>{value}%</p>
+//                             <div
+//                                 className={cn(`h-full`, color)}
+//                                 style={{
+//                                     width: `${value}%`,
+//                                     backgroundImage: `linear-gradient(120deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent)`,
+//                                     backgroundSize: `200% 100%`,
+//                                     transition: `background-position-x 1s ease`,
+//                                     animation: `progress-bar-stripes 6s linear infinite`,
+//                                 }}
+//                             />
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//             <a href={linkCasa} target='_blank' className="mb-2 mx-2 hover:underline border border-zinc-50/15 text-center justify-center bg-zinc-50/10 hover:bg-zinc-50/20 items-center flex font-bold rounded-md transition-all text-base sm:text-base">
+//                 <p className="text-zinc-50">JOGUE AGORA</p>
+//             </a>
+//             <div className="w-full h-full backdrop-blur-md pt-2 pb-1 px-2 mt-2 flex flex-col items-center justify-center text-nowrap">
+//                 {[
+//                     { label: 'Mínima R$0,50 a R$2,40' },
+//                     { label: 'Padrão R$2,50 a R$10,00' },
+//                     { label: 'Máxima acima de R$10,00' },
+//                 ].map(({ label }) => (
+//                     <p key={label} className="text-start text-xs flex gap-1 items-center justify-start">
+//                         <MoneyWavy /> {label}
+//                     </p>
+//                 ))}
+//             </div>
+//             {isHot && (
+//                 <div className="w-full items-center flex justify-center py-2 px-1">
+//                     <Link
+//                         href={`/gerar-sinais/${nomeJogo}`}
+//                         className="bg-red-700 text-zinc-50 font-bold py-3 px-1 rounded-xl hover:bg-red-700 w-full text-center text-nowrap text-sm sm:text-base animate-pulse">
+//                         Estratégia detectada
+//                     </Link>
+//                 </div>
+//             )}
+//         </div>
+//     </div>
+// </div >
