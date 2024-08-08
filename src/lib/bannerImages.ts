079f652a79fa1557665909e04bf8bc1c258ca26e
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-08-08 19:56:44 -0300

    feat: V5 new images

--- src/lib/bannerImages.ts
+++ src/lib/bannerImages.ts
@@ -1,13 +1,13 @@
 export const bannerImages = [
     {
-        mobile: '/image/banner/mobile/teste1.png',
-        desktop: '/image/banner/desktop/teste1.png'
+        mobile: '/image/banner/mobile/1.jpeg',
+        desktop: '/image/banner/desktop/1.jpeg'
     },
     {
-        mobile: '/image/banner/mobile/teste2.png',
-        desktop: '/image/banner/desktop/teste02.png',
+        mobile: '/image/banner/mobile/2.jpeg',
+        desktop: '/image/banner/desktop/2.jpeg',
     },
     {
-        mobile: '/image/banner/mobile/teste3.png',
-        desktop: '/image/banner/desktop/teste03.png',
+        mobile: '/image/banner/mobile/3.jpeg',
+        desktop: '/image/banner/desktop/3.jpeg',
     }];
