6b7eb474346f2e0da18a435805b1a2849078b349
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-08-13 21:17:35 -0300

    v6 teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import { ButtonScrollTop } from "@/components/button-scroll-top";
 import { getCardsPG, getLinkCasa } from "@/lib/actions";
 import { ContentPg } from "@/components/content-pg";
@@ -9,21 +8,19 @@ import { Analytics } from "@vercel/analytics/next";
 export default async function Home() {
   const cards = await getCardsPG();
   const propsSettings = await getLinkCasa();
-  const novoLink = propsSettings.data?.link
-  const imageBanner = propsSettings.data?.bannerImage
+  const novoLink = propsSettings.data?.link;
+  const imageBanner = propsSettings.data?.bannerImage;
 
-  let horario = cards?.data[0].updatedAt || ''
+  let horario = cards?.data[0].updatedAt || '';
   const date = new Date(horario);
   
-  const timeZone = 'America/Sao_Paulo'
-  const zonedDate = toZonedTime(date, timeZone)
-  const formattedDate = format(zonedDate, 'HH:mm', { timeZone })
+  const timeZone = 'America/Sao_Paulo';
+  const zonedDate = toZonedTime(date, timeZone);
+  const formattedDate = format(zonedDate, 'HH:mm:ss', { timeZone });
 
- 
   return (
     <main>
       <Analytics />
-  
       <ContentPg updateTime={formattedDate} imageBanner={imageBanner} />
       <SectionCards cards={cards?.data as CardData[]} linkCasa={novoLink} />
     </main>
