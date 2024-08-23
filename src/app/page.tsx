b2734ebbe42545d0cf5cab3bd9b24e8c3b389e57
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-08-22 23:06:38 -0300

    fix effect initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,25 @@
-'use client'
-import { Suspense, useState, useEffect } from 'react';
 import { getCardsPG, getLinkCasa } from "@/lib/actions";
 import { ContentPg } from "@/components/content-pg";
 import { CardData, SectionCards } from "@/components/section-cards-pg";
 import { formatUpdateTime } from "@/lib/utils";
 import Loading from './loading';
 
-function HomePage() {
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [cards, setCards] = useState<CardData[] | null>(null);
-  const [linkCasa, setLinkCasa] = useState<string | null>(null);
-  const [imageBanner, setImageBanner] = useState<string | null>(null);
-  const [updateTime, setUpdateTime] = useState<string>('');
+async function loadData() {
+  const cardsData = await getCardsPG();
+  const propsSettings = await getLinkCasa();
 
-  useEffect(() => {
-    async function loadData() {
-      const cardsData = await getCardsPG();
-      const propsSettings = await getLinkCasa();
-      
-      setCards(cardsData?.data as CardData[]);
-      setLinkCasa(propsSettings.data?.link || null);
-      setImageBanner(propsSettings.data?.bannerImage || null);
-      setUpdateTime(cardsData?.data[0]?.updatedAt ? formatUpdateTime(cardsData.data[0].updatedAt) : '');
+  const cards = cardsData?.data as CardData[];
+  const linkCasa = propsSettings.data?.link || null;
+  const imageBanner = propsSettings.data?.bannerImage || null;
+  const updateTime = cardsData?.data[0]?.updatedAt ? formatUpdateTime(cardsData.data[0].updatedAt) : '';
 
-      setDataLoaded(true);
-    }
+  return { cards, linkCasa, imageBanner, updateTime };
+}
 
-    loadData();
-  }, []);
+export default async function HomePage() {
+  const { cards, linkCasa, imageBanner, updateTime } = await loadData();
 
-  if (!dataLoaded) {
+  if (!cards || !linkCasa || !imageBanner || !updateTime) {
     return <Loading />;
   }
 
@@ -38,13 +28,5 @@ function HomePage() {
       <ContentPg updateTime={updateTime} imageBanner={imageBanner} />
       <SectionCards cards={cards} linkCasa={linkCasa} />
     </main>
-  );
-}
-
-export default function Home() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <HomePage />
-    </Suspense>
   );
 }
