32ba610e0bd64887fa39b81c6e1663d8cec32d38
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-08-22 22:54:43 -0300

    fix timer com cron

--- src/components/content-pg.tsx
+++ src/components/content-pg.tsx
@@ -9,7 +9,6 @@ import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { bannerImages } from '@/lib/bannerImages';
 import { ReloadIcon } from '@radix-ui/react-icons';
-import { useRouter } from 'next/navigation';
 
 export interface ContentPgProps {
     updateTime: string | number | undefined;
@@ -20,13 +19,10 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
     const [showPopup, setShowPopup] = useState<boolean>(false);
     const [isClient, setIsClient] = useState<boolean>(false);
     const [timeLeft, setTimeLeft] = useState<number>(0);
-    const [isUpdating, setIsUpdating] = useState<boolean>(false);
-    const [updateTime, setUpdateTime] = useState<string | number | undefined>(initialUpdateTime);
-    const router = useRouter();
 
     useEffect(() => {
         setIsClient(true);
-
+        
         if (isClient) {
             const storedPopupShown = localStorage.getItem('popupShown');
             const storedImageBanner = localStorage.getItem('lastImageBanner');
@@ -49,9 +45,9 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
     }, [imageBanner]);
 
     const calculateTimeLeft = useCallback(() => {
-        if (typeof updateTime === 'string') {
+        if (typeof initialUpdateTime === 'string') {
             const now = new Date();
-            const [hours, minutes, seconds] = updateTime.split(':').map(Number);
+            const [hours, minutes, seconds] = initialUpdateTime.split(':').map(Number);
             const lastUpdate = new Date(now);
 
             lastUpdate.setHours(hours, minutes, seconds, 0);
@@ -65,16 +61,7 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
             const timeDifference = nextUpdate.getTime() - now.getTime();
             setTimeLeft(timeDifference > 0 ? timeDifference : 0);
         }
-    }, [updateTime]);
-
-    const refreshUpdateTime = useCallback(() => {
-        setIsUpdating(true);
-        const now = new Date();
-        const formattedTime = now.toTimeString().split(' ')[0];
-        setUpdateTime(formattedTime);
-        calculateTimeLeft();
-        setIsUpdating(false);
-    }, [calculateTimeLeft]);
+    }, [initialUpdateTime]);
 
     useEffect(() => {
         calculateTimeLeft();
@@ -82,8 +69,7 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
         const timer = setInterval(() => {
             setTimeLeft((prevTime) => {
                 if (prevTime <= 1000) {
-                    setIsUpdating(true);
-                    refreshUpdateTime();
+                    calculateTimeLeft();
                     return 0;
                 } else if (prevTime > 0) {
                     return prevTime - 1000;
@@ -94,10 +80,9 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
         }, 1000);
 
         return () => clearInterval(timer);
-    }, [calculateTimeLeft, refreshUpdateTime]);
+    }, [calculateTimeLeft]);
 
     const formatTime = (ms: number) => {
-        if (isUpdating) return 'BUSCANDO DADOS';
         if (ms <= 0) return '0s';
         const seconds = Math.floor((ms / 1000) % 60);
         const minutes = Math.floor((ms / (1000 * 60)) % 60);
@@ -149,17 +134,11 @@ export function ContentPg({ updateTime: initialUpdateTime, imageBanner }: Conten
 
                 <div className="flex flex-col items-center justify-center max-w-[600px] w-full rounded-2xl p-5 bg-gradient-to-b to-green-800 via-green-600 from-green-500 shadow-xl shadow-black">
                     <h1 className="text-base uppercase font-bold">
-                        Última atualização às {updateTime}
+                        Última atualização às {initialUpdateTime}
                     </h1>
-                    {isUpdating ? (
-                        <p className='text-xs sm:text-base flex flex-row gap-2 font-bold items-center justify-center'>
-                            {formatTime(timeLeft)} <ReloadIcon className='animate-spin text-lg font-bold' />
-                        </p>
-                    ) : (
-                        <p className='text-xs sm:text-base'>
-                            Próxima atualização em: {formatTime(timeLeft)}
-                        </p>
-                    )}
+                    <p className='text-xs sm:text-base'>
+                        Próxima atualização em: {formatTime(timeLeft)}
+                    </p>
                 </div>
             </div>
         </>
