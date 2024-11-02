c19b5b1cee574445f424c99f0a2cdfe8207ea10b
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-02 16:50:49 -0300

    fiz navbar + gerar sinais

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -7,7 +7,6 @@ export async function NavBar() {
     const novoLink = getLink.data?.link
 
     return (
-
         <nav className="shadow-xl bg-yellow-fp border-b border-zinc-900/20 fixed top-0 w-full z-50">
             <div className="container mx-auto flex flex-col items-center justify-center gap-5 py-3 px-2">
                 <div className="flex flex-row gap-2 sm:gap-10 text-lg w-full justify-center items-center">
@@ -19,10 +18,10 @@ export async function NavBar() {
                         <p className="text-yellow-fp text-xs sm:text-sm">JOGOS PP</p>
                     </Link>
 
-                    {/* <div className="h-8 border-r border-zinc-50/10" />
+                    <div className="h-8 border-r border-zinc-50/10" />
                     <Link href="/gerar-sinais" className="text-center justify-center items-center flex bg-green-fp hover:bg-green-700 py-2 px-2 font-bold rounded-md transition-all duration-10  sm:text-base">
                         <p className="text-yellow-fp text-xs sm:text-sm">GERAR SINAIS</p>
-                    </Link> */}
+                    </Link>
                     <div className="h-8 border-r border-zinc-50/10" />
                     {novoLink ? (
                         <Link href={novoLink} target='_blank' className="h-full text-xs sm:text-sm italic font-bold py-2 px-2 text-center bg-green-600 hover:bg-green-500 text-yellow-300 rounded-md transition-all duration-200">
