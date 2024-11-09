09c0ae210549f0624f27de47ddc7955e3722aac8
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-09 13:58:34 -0300

    fix in pwa

--- src/components/download-app-button.tsx
+++ src/components/download-app-button.tsx
@@ -12,10 +12,14 @@ export function DownloadAppButton() {
   const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
+    const checkPWA = () => {
+      const isStandalone = window.matchMedia('(display-mode: standalone)').matches
+      const isFullscreen = 'standalone' in window.navigator && window.navigator['standalone']
+      return isStandalone || isFullscreen
+    }
 
-    setIsVisible(true)
+    setIsVisible(!checkPWA())
   }, [])
-
 
   if (HIDDEN_PATHS.includes(pathname) || pathname.startsWith('/pwa/') || !isVisible) {
     return null
@@ -31,14 +35,14 @@ export function DownloadAppButton() {
     <div className="fixed bottom-6 right-6 z-50 flex items-center">
       <Link
         href="/download-app"
-        className="text-sm relative flex items-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-3 px-6 rounded-full shadow-lg transform transition-all hover:scale-105 duration-700"
+        className="text-xs relative flex items-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-3 rounded-full shadow-lg transform transition-all hover:scale-105 duration-700"
       >
-        <Download className="w-5 h-5" />
+        <Download className="w-4 h-4" />
         Baixe nosso app
       </Link>
       <button
         onClick={handleDismiss}
-        className="ml-2 text-gray-50 rounded-full p-2 transition-all duration-300 hover:scale-110 absolute -top-10 right-2"
+        className="ml-2 text-gray-50 rounded-full p-2 transition-all duration-300 hover:scale-110 absolute -top-7 right-0"
         aria-label="Fechar botão de download"
       >
         <X className="w-4 h-4" />
