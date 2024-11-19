b1dee9be4a5d8140f56dfe4562193b0adfaaa665
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-19 00:11:42 -0300

    banner images

--- src/lib/bannerImages.ts
+++ src/lib/bannerImages.ts
@@ -1,18 +1,18 @@
 export const bannerImages = [
     {
-        mobile: '/image/banner/mobile/natal/3.png',
+        mobile: '/image/banner/mobile/natal/01.png',
         desktop: '/image/banner/desktop/natal/1.png'
     },
     {
-        mobile: '/image/banner/mobile/natal/1.png',
+        mobile: '/image/banner/mobile/natal/02.png',
         desktop: '/image/banner/desktop/natal/2.png',
     },
     {
-        mobile: '/image/banner/mobile/natal/2.png',
+        mobile: '/image/banner/mobile/natal/03.png',
         desktop: '/image/banner/desktop/natal/3.png',
     },
     {
-        mobile: '/image/banner/mobile/natal/3.png',
+        mobile: '/image/banner/mobile/natal/04.png',
         desktop: '/image/banner/desktop/natal/4.png',
     }
 ];
