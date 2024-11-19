4ab4621f570f11b1f15fe8d2c6f0fea76d886ea3
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-19 17:22:49 -0300

    ppgames

--- src/app/(pages)/pp-games/page.tsx
+++ src/app/(pages)/pp-games/page.tsx
@@ -1,32 +1,21 @@
+
 import { ButtonScrollTop } from "@/components/button-scroll-top";
 import { ContentPp } from "@/components/content-pp";
 import { CardData } from "@/components/section-cards-pg";
 import { SectionCardsPP } from "@/components/section-cards-pp";
 import { getCardsPP, getLinkCasa } from "@/lib/actions";
 import { formatUpdateTime } from "@/lib/utils";
-import Loading from "@/app/loading";
-import { NavBar } from "@/components/nav-bar";
-import { Footer } from "@/components/footer";
-import { cardsCache } from "@/lib/cache"; // Importa o cache
+import Loading from '@/app/loading';
+import { NavBar } from '@/components/nav-bar';
+import { Footer } from '@/components/footer';
 
 async function loadData() {
-    let cards = cardsCache.pp || null; // Tenta buscar do cache em memória
-    let linkCasa = cardsCache.linkCasa?.link || null; // Tenta buscar do cache em memória
-    let updateTime = cardsCache.pp?.[0]?.updatedAt
-        ? formatUpdateTime(cardsCache.pp[0].updatedAt)
-        : "";
+    const cardsData = await getCardsPP();
+    const propsSettings = await getLinkCasa();
 
-    // Caso o cache em memória não tenha os dados, busca do banco
-    if (!cards || !linkCasa) {
-        const cardsData = await getCardsPP();
-        const propsSettings = await getLinkCasa();
-
-        cards = cardsData?.data as CardData[];
-        linkCasa = propsSettings.data?.link || null;
-        updateTime = cardsData?.data[0]?.updatedAt
-            ? formatUpdateTime(cardsData.data[0].updatedAt)
-            : "";
-    }
+    const cards = cardsData?.data as CardData[];
+    const linkCasa = propsSettings.data?.link || null;
+    const updateTime = cardsData?.data[0]?.updatedAt ? formatUpdateTime(cardsData.data[0].updatedAt) : '';
 
     return { cards, linkCasa, updateTime };
 }
@@ -41,11 +30,11 @@ export default async function PpGamesPage() {
     return (
         <>
             <NavBar />
-            <main>
-                <ButtonScrollTop />
-                <ContentPp />
-                <SectionCardsPP cards={cards} linkCasa={linkCasa} />
-            </main>
+                <main>
+                    <ButtonScrollTop />
+                    <ContentPp/>
+                    <SectionCardsPP cards={cards} linkCasa={linkCasa} />
+                </main>
             <Footer />
         </>
     );
