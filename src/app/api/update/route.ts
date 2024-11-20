e3fb19a56997d8b9e8cc8ab27a4708a7a2102907
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-20 13:55:05 -0300

    fix ajust route.ts

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -4,37 +4,27 @@ import { updateCards } from '@/lib/actions';
 export async function GET() {
   try {
     // Atualize os dados no banco de dados
-    await updateCards();
+    const updateResult = await updateCards();
 
-    // // Revalidação do cache (via Cloudflare, se necessário)
-    // const cloudflareResponse = await fetch(`https://api.cloudflare.com/client/v4/zones/d1904632fbeb9a8db0ed41324a6188aa/purge_cache`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Authorization': `Bearer 97dbe17bfec74e57e223a79e7dfec71370269`,
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     files: ['https://www.grupofpsinais.com/api/update'], // A própria rota
-    //   }),
-    // });
-
-    // if (!cloudflareResponse.ok) {
-    //   const errorDetails = await cloudflareResponse.json();
-    //   console.error('Erro ao limpar cache do Cloudflare:', errorDetails);
-    //   return NextResponse.json(
-    //     { success: false, message: 'Erro ao limpar cache no Cloudflare' },
-    //     { status: 500 }
-    //   );
-    // }
-
-    // Retorne os dados ao frontend
-    return NextResponse.json(
-      {
-        success: true,
-        message: 'Dados atualizados com sucesso e cache revalidado',
-      },
-      { status: 200 }
-    );
+    if (updateResult.success) {
+      // Retorne uma mensagem de sucesso
+      return NextResponse.json(
+        {
+          success: true,
+          message: 'Dados atualizados com sucesso e cache revalidado.',
+        },
+        { status: 200 }
+      );
+    } else {
+      // Retorne uma mensagem de erro
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Erro ao atualizar os dados',
+        },
+        { status: 500 }
+      );
+    }
   } catch (error) {
     console.error('Error updating cards:', error);
     return NextResponse.error();
