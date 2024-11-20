1701b8421cf0077e468e98969b2342cfea7273b8
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-11-19 23:34:51 -0300

    fix: page pp

--- src/app/(pages)/pp-games/page.tsx
+++ src/app/(pages)/pp-games/page.tsx
@@ -1,56 +1,41 @@
-import { getCardsPG, getLinkCasa } from "@/lib/actions";
-import { ContentPg } from "@/components/content-pg";
-import { CardData, SectionCards } from "@/components/section-cards-pg";
+
+import { ButtonScrollTop } from "@/components/button-scroll-top";
+import { ContentPp } from "@/components/content-pp";
+import { CardData } from "@/components/section-cards-pg";
+import { SectionCardsPP } from "@/components/section-cards-pp";
+import { getCardsPP, getLinkCasa } from "@/lib/actions";
 import { formatUpdateTime } from "@/lib/utils";
 import Loading from '@/components/loading';
-import { NavBar } from "@/components/nav-bar";
-import { Footer } from "@/components/footer";
-
-const CACHE_EXPIRATION_TIME = 300000; // 5 minutos em milissegundos
-let cachedData: { cards: CardData[]; linkCasa: string; imageBanner: string; updateTime: string } | null = null;
-let cacheTimestamp: number = 0;
+import { NavBar } from '@/components/nav-bar';
+import { Footer } from '@/components/footer';
 
 async function loadData() {
-  const now = Date.now();
+    const cardsData = await getCardsPP();
+    const propsSettings = await getLinkCasa();
 
-  // Verifica se o cache existe e não está expirado
-  if (cachedData && (now - cacheTimestamp) < CACHE_EXPIRATION_TIME) {
-    return cachedData;
-  }
+    const cards = cardsData?.data as CardData[];
+    const linkCasa = propsSettings.data?.link || null;
+    const updateTime = cardsData?.data[0]?.updatedAt ? formatUpdateTime(cardsData.data[0].updatedAt) : '';
 
-  // Se o cache estiver expirado, recarrega os dados
-  const cardsData = await getCardsPG();
-  const propsSettings = await getLinkCasa();
-
-  const cards = cardsData?.data as CardData[];
-  const linkCasa = propsSettings.data?.link || '';
-  const imageBanner = propsSettings.data?.bannerImage || '';
-  const updateTime = cardsData?.data[0]?.updatedAt ? formatUpdateTime(cardsData.data[0].updatedAt) : '';
-
-  // Atualiza o cache e o timestamp
-  cachedData = { cards, linkCasa, imageBanner, updateTime };
-  cacheTimestamp = now;
-
-  return cachedData;
+    return { cards, linkCasa, updateTime };
 }
 
-export default async function HomePage() {
-  const data = await loadData();
+export default async function PpGamesPage() {
+    const { cards, linkCasa, updateTime } = await loadData();
 
-  if (!data || !data.cards || !data.linkCasa || !data.imageBanner || !data.updateTime) {
-    return <Loading />;
-  }
+    if (!cards || !linkCasa || !updateTime) {
+        return <Loading />;
+    }
 
-  const { cards, linkCasa, imageBanner, updateTime } = data;
-
-  return (
-    <>
-    <NavBar />
-      <main>
-        <ContentPg updateTime={updateTime} imageBanner={imageBanner} />
-        <SectionCards cards={cards} linkCasa={linkCasa} />
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+                <main>
+                    <ButtonScrollTop />
+                    <ContentPp/>
+                    <SectionCardsPP cards={cards} linkCasa={linkCasa} />
+                </main>
+            <Footer />
+        </>
+    );
 }
